b450dbd09994d290a8049834c85fe5e7dcdb6ab5
Fábio Barros <67093379+fpbbarros2@users.noreply.github.com>
2024-02-27 21:21:44 -0300

    como customizar a seralização de um objeto

--- RestWithAspNetOk/RestWithAspNet5UdemayErudio/RestWithAspNet5UdemayErudio/Data/Vo/PersonVo.cs
+++ RestWithAspNetOk/RestWithAspNet5UdemayErudio/RestWithAspNet5UdemayErudio/Data/Vo/PersonVo.cs
@@ -1,11 +1,22 @@
-﻿namespace RestWithAspNet5UdemayErudio.Data.Vo
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithAspNet5UdemayErudio.Data.Vo
 {
     public class PersonVo
     {
+        [JsonPropertyName("id")]
         public long Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+
+        [JsonIgnore]
         public string Address { get; set; }
+
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
